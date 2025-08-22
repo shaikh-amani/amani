23c7575acfe2dee3e611e851d28505ba4d349c7d
Amani shaikh <shaikhammu019@gmail.com>
2025-08-22 16:20:42 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,12 @@
+public class p1
+{
+	public static void main(String []args)
+	{
+		System.out.println("Enter number:"+args[0]);
+		int no=Integer.parseInt(args[0]);
+		int sum=no+1;
+		System.out.println("sum is:"+sum);
+	}
+}
+
+
